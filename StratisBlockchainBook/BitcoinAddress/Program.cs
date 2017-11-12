b356c5c12e42d5fc9f4f754f55c1b4416d83c33d
youngmtool <youngmtool@gmail.com>
2017-11-12 23:34:45 +0900

    Update 1.1

--- StratisBlockchainBook/BitcoinAddress/Program.cs
+++ StratisBlockchainBook/BitcoinAddress/Program.cs
@@ -12,23 +12,34 @@ namespace BitcoinAddress
         static void Main()
         {
             RandomUtils.Random = new UnsecureRandom();
-            Key privateKey = new Key(); // generate a random private key
 
+            // generate a random private key
+            Key privateKey = new Key();
+            Console.WriteLine(privateKey);
+
+            //On the private key, I use a one-way cryptographic function, to generate a public key.
             PubKey publicKey = privateKey.PubKey;
-            Console.WriteLine(publicKey); // 0251036303164f6c458e9f7abecb4e55e5ce9ec2b2f1d06d633c9653a07976560c
+            Console.WriteLine(publicKey);
 
+            //I can generate public bitcoin address by using publickey and network type. Note that public address for mainnet starts with "1", public address for testnet starts with "m".
             Console.WriteLine(publicKey.GetAddress(Network.Main)); // 1PUYsjwfNmX64wS368ZR5FMouTtUmvtmTY
             Console.WriteLine(publicKey.GetAddress(Network.TestNet)); // n3zWAo2eBnxLr3ueohXnuAa8mTVBhxmPhq
 
+
+            //To be precise, a bitcoin address is made of a version byte(which is different from the both networks) and your public key's hash bytes. Both of these bytes are concatenated and then encoded into a Base58Check.
+            //Underlying, publickey is hashed twice, first it's hashed by SHA256, second on its result, it's hashed by RIPEMD160 with using Big Endian notation.
+            //The process can be expressed like this:
+            //RIPEMD160(SHA256(publickey))
             var publicKeyHash = publicKey.Hash;
-            Console.WriteLine(publicKeyHash); // f6889b21b5540353a29ed18c45ea0031280c42cf
+            // f6889b21b5540353a29ed18c45ea0031280c42cf
+            Console.WriteLine(publicKeyHash); 
+            
+            //Underlying this statement, publickeyHash(bytes), a version byte are concatenated and then encoded into a Base58Check. 
             var mainNetAddress = publicKeyHash.GetAddress(Network.Main);
             var testNetAddress = publicKeyHash.GetAddress(Network.TestNet);
 
             Console.WriteLine(mainNetAddress); // 1PUYsjwfNmX64wS368ZR5FMouTtUmvtmTY
             Console.WriteLine(testNetAddress); // n3zWAo2eBnxLr3ueohXnuAa8mTVBhxmPhq
-
-            Console.ReadLine();
         }
     }
 }
