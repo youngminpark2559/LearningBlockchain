19d30cd5e5a19a8a68ce31d896b727648b4e11e0
youngmtool <youngmtool@gmail.com>
2017-11-15 17:03:23 +0900

    Update 4.2LiquidDemocracy

--- NBitcoinBlockchainBook/4.2LiquidDemocracy/Program.cs
+++ NBitcoinBlockchainBook/4.2LiquidDemocracy/Program.cs
@@ -38,9 +38,8 @@ namespace _4._2LiquidDemocracy
 
 
 
-
             //===========================================================================================
-            //Section1. Issuing voting power
+            //Section2. Issuing voting power
 
             //Everything starts with the founder of the company(let’s call him Boss) wanting to sell “decision power” in his company to some investors. The decision power can take the shape of a colored coin that we will call for the sake of this exercise a “Power Coin”.
 
@@ -101,11 +100,18 @@ namespace _4._2LiquidDemocracy
 
             //For some reason, Alice, might want to sell some of her voting power to Satoshi.
 
+            //You can note that I am double spending the coin of Alice from the init transaction.
+            //**Such thing would not be accepted on the Blockchain. However, we have not seen yet how to retrieve unspent coins from the Blockchain easily, so let’s just imagine for the sake of the exercise that the coin was not double spent.
+            // Now that Alice and Satoshi have some voting power, let’s see how Boss can run a vote.
+
+            //===========================================================================================
+            //Section3. Running a vote
+
+            //By consulting the Blockchain, Boss can at any time know ScriptPubKeys which own Power Coins.
+            //So he will send Voting Coins to these owners, proportionally to their voting power, in our case, 1 voting coin to Alice and 1 voting coin to Satoshi.
 
 
-
-
-
+            //First, I need to create some funds for votingCoin.
             var votingCoin = new Key();
             var init2 = new Transaction()
             {
@@ -116,6 +122,7 @@ namespace _4._2LiquidDemocracy
             };
             repo.Transactions.Put(init2);
 
+            //Then, issue the voting coins.
             issuance = GetCoins(init2, votingCoin).Select(c => new IssuanceCoin(c)).ToArray();
             builder = new TransactionBuilder();
             var toVoters =
@@ -127,6 +134,13 @@ namespace _4._2LiquidDemocracy
                 .SetChange(votingCoin)
                 .BuildTransaction(true);
             repo.Transactions.Put(toVoters);
+
+
+            //===========================================================================================
+            //Section4. Vote delegation
+
+            //The problem is that the vote concerns some financial aspect of the business, and Alice is mostly concerned by the marketing aspect.
+            //Her decision is to handout her voting coin to someone she trusts having a better judgment on financial matter. She chooses to delegate her vote to Bob.
 
             var aliceVotingCoin = ColoredCoin.Find(toVoters, repo)
                         .Where(c => c.ScriptPubKey == alice.ScriptPubKey)
@@ -140,24 +154,50 @@ namespace _4._2LiquidDemocracy
                 .BuildTransaction(true);
             repo.Transactions.Put(toBob);
 
+            //You can notice that there is no SetChange() extension method. The reason for that is that the input colored coin is spent entirely, so that nothing is left to be returned.
+
+
+            //===========================================================================================
+            //Section5. Voting
+
+            //Imagine that Satoshi is too busy and decides not to vote.Now Bob must express his decision.
+            //The vote concerns whether the company should ask for a loan to the bank for investing into new production machines.
+            //Boss says on the company’s website:
+            //Send your coins to 1HZwkjkeaoZfTSaJxDw6aKkxp45agDiEzN for "Yes" or to 1F3sAm6ZtwLAUnj7d38pGFxtP3RVEvtsbV for "No".
+            //Bob decides that the company should take the loan:
+
             var bobVotingCoin = ColoredCoin.Find(toVoters, repo)
                         .Where(c => c.ScriptPubKey == bob.ScriptPubKey)
                         .ToArray();
 
             builder = new TransactionBuilder();
-            var vote =
-                builder
+            var vote = builder
                 .AddCoins(bobVotingCoin)
                 .AddKeys(bob)
                 .SendAsset(BitcoinAddress.Create("1HZwkjkeaoZfTSaJxDw6aKkxp45agDiEzN"),
                             new AssetMoney(votingCoin, 1))
                 .BuildTransaction(true);
 
+            //Now Boss can compute the result of the vote and see 1-Yes 0-No. 
+            //Yes and he wins, so he takes the loan.
+            //Every participants can also count the result by themselves.
+
+
+
+
+            //===========================================================================================
+            //Section6. Alternative: Use of Ricardian Contract
+
+            //In the previous exercise, we have supposed that Boss announced the modalities of the vote, out of the Blockchain, on the company’s website.
+            //This works great, but Bob need to know that the website exists.
+            //Another solution for this existing website problem is to publish the modalities of the vote directly on the Blockchain within an Asset Definition File, so some software can automatically get it and present it to Bob.
+            //The only piece of code that would have changed is during the issuance of the Voting Coins to voters.
+
+
             issuance = GetCoins(init2, votingCoin).Select(c => new IssuanceCoin(c)).ToArray();
             issuance[0].DefinitionUrl = new Uri("http://boss.com/vote01.json");
             builder = new TransactionBuilder();
-            toVoters =
-                builder
+            toVoters = builder
                 .AddCoins(issuance)
                 .AddKeys(votingCoin)
                 .IssueAsset(alice, new AssetMoney(votingCoin, 1))
@@ -166,7 +206,14 @@ namespace _4._2LiquidDemocracy
                 .BuildTransaction(true);
             repo.Transactions.Put(toVoters);
 
-            Console.ReadLine();
+
+
+            //In such case, Bob can see that during the issuance of his voting coin, an Asset Definition File was published, which is nothing more than a JSON document whose schema is partially specified in Open Asset.The schema can be extended to have information about things like:
+            //1.Expiration of the vote
+            //2.Destination of the votes for each candidates
+            //3.Human friendly description of it
+            //However, imagine that a hacker wants to cheat the vote. He can always modify the JSON document(either man in the middle of attack, physical access to boss.com, or access to Bob’s machine) so Bob is tricked and sends his vote to the wrong candidate.
+            //Transforming the Asset Definition File into a Ricardian Contract by signing it would make any modification immediately detectable by Bob’s software. (See Proof Of Authenticity in the Asset Definition Protocol)
         }
 
         private static IEnumerable<Coin> GetCoins(Transaction tx, Key owner)
